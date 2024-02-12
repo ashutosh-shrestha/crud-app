8d0cb38baccb7d6ea8e8ec1e918b6100c5f658c0
ashutoshshrestha <ashu0313stha@gmail.com>
2024-02-12 17:08:36 -0600

    Switch from postgres to H2. Update schema and data files. Remove postgres dependency.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,19 +7,45 @@ INSERT INTO person (
     state,
     zip_code
 ) VALUES (
-    'John',
-    'Smith',
-    'fake1@aquent.com',
-    '123 Any St.',
-    'Asheville',
-    'NC',
-    '28801'
+    'Christopher',
+    'Nolan',
+    'cNolan@test.com',
+    '44 Dark Knight ln.',
+    'New York',
+    'NY',
+    '02020'
 ), (
-    'Jane',
-    'Smith',
-    'fake2@aquent.com',
-    '123 Any St.',
-    'Asheville',
-    'NC',
-    '28801'
+    'Quentin',
+    'Tarantino',
+    'qTarantino@test.com',
+    '55 Django St.',
+    'San Francisco',
+    'CA',
+    '22333'
 );
+
+INSERT INTO client (
+    client_name,
+    client_phone,
+    client_uri,
+    street_address,
+    city,
+    state,
+    zip_code
+) VALUES (
+    'Dream Works',
+    '44445555666',
+    'https://www.dreamworks.com',
+    '11 Santander St.',
+    'Chicago',
+    'IL',
+    '22345'
+), (
+    'Disney',
+    '23623623623',
+    'https://www.disney.com',
+    '14102 Centreville Rd',
+    'Houston',
+    'TX',
+    '22204'
+)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,24 @@
+DROP TABLE IF EXISTS person;
+
 CREATE TABLE person (
     person_id integer IDENTITY,
+    client_id integer DEFAULT -1,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
-    email_address varchar(50) NOT NULL,
+    email_address varchar(50) NOT NULL UNIQUE,
+    street_address varchar(50) NOT NULL,
+    city varchar(50) NOT NULL,
+    state varchar(2) NOT NULL,
+    zip_code varchar(5) NOT NULL
+);
+
+DROP TABLE IF EXISTS client;
+
+CREATE TABLE client(
+    client_id integer IDENTITY,
+    client_name varchar(50) NOT NULL,
+    client_phone varchar(15) NOT NULL UNIQUE,
+    client_uri varchar(50) NOT NULL UNIQUE,
     street_address varchar(50) NOT NULL,
     city varchar(50) NOT NULL,
     state varchar(2) NOT NULL,
